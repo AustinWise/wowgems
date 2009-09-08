c92df6ec419b129688c79b3549bce866411e66dd
unknown <William  Archibald@.(none)>
2009-09-08 14:24:39 -0700

    Fixed templete and added other gems.

--- wowgems.rb
+++ wowgems.rb
@@ -4,10 +4,26 @@ require 'xml'
 
 get '/' do
   doc = XML::Document.file('Gems.xml')
+  
+  @jewelry = []
+  doc.find('//gems/wrath/jewelry/item').each do |g|
+    @jewelry << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
+  end
+  
+  @metaGems = []
+  doc.find('//gems/wrath/meta/gem').each do |g|
+    @metaGems << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
+  end
+  
   @epicGems = []
   doc.find('//gems/wrath/epic/gem').each do |g|
     @epicGems << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
   end
+  
+  @rareGems = []
+  doc.find('//gems/wrath/rare/gem').each do |g|
+    @rareGems << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
+  end
+  
   erb :gems
 end
-
