f3bf0715f0ed3ce1d89aa6df439a663912c59c28
unknown <William  Archibald@.(none)>
2009-09-08 14:08:39 -0700

    First working version.

--- wowgems.rb
+++ wowgems.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
-get '/hi' do
-  "Hello World!"
+require 'xml'
+
+get '/' do
+  doc = XML::Document.file('Gems.xml')
+  @epicGems = []
+  doc.find('//gems/wrath/epic/gem').each do |g|
+    @epicGems << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
+  end
+  erb :gems
 end
 
